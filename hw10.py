6011397b8666fb55751109c531528eaf777f795d
ERIC ADAMS <ericadams@ERICs-MacBook-Pro.local>
2017-03-01 13:28:27 -0800

    final homework assignment

---
+++ hw10.py
@@ -0,0 +1,9 @@
+# Update the test case automation road map with new test cases and versions downloaded from a repository
+# Read in test case file names and version from a csv file
+# open the road map using pandas
+# Compare test cases, (file names), with what is on the Sync tab, column E , row 15 - end of sheet
+# Compare the version with what is on the Sync tab, column P
+# If the test case is on the road map and the version matches, mark column R with an x to indicate that this
+#   test is in the test case text file
+# If the test case is not on the road map  add it to the end of column E on the Sync sheet and add the version
+#    to column P
