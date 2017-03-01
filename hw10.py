43450c43ac24374c6904e6decd9e5388d3eaa850
ea8606 <theadamsfamily11@msn.com>
2017-03-01 14:20:14 -0800

    Update hw10.py

--- hw10.py
+++ hw10.py
@@ -8,4 +8,4 @@
 # If the test case is on the road map and the version matches, mark column R with an x to indicate that this
 #   test is in the test case text file
 # If the test case is not on the road map  add it to the end of column E on the Sync sheet and add the version
-#    to column P
+#    to column O
