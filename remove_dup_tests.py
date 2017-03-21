62f3b5966d37b670a617c56438bb7cbbcc1c50bc
ea8606 <theadamsfamily11@msn.com>
2017-03-21 13:38:26 -0700

    Update remove_dup_tests.py

--- remove_dup_tests.py
+++ remove_dup_tests.py
@@ -1,6 +1,9 @@
 # open roadmap 
 # for sheets, "async","sync, "manual intervention", "User Interaction(GUI)", "Out of Scoped"
-# count = 1
-# for rows 15 to 5000
-#    if cell(E15) and cell(O15) equals( cell(E(15 + count) and cell(O15 + count))
-#        print the row in a separate sheet(this row is a duplicate)
+#     count1 = 0
+#     count2 = 1
+#     for rows 15 to 5000
+#         if (cell(E(15 + count1)) and cell(O(15 + count1)) equals( cell(E(15 + count2) and cell(O(15 + count2)))
+#         print the row in a separate sheet(this row is a duplicate)
+#         count1 +=1
+#         count2 +=1
